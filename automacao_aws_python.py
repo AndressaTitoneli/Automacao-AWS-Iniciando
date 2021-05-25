cc3bbc759fd7a3fccaef6a580f853d33a3cc0e33
Andressa Titoneli <48228070+AndressaTitoneli@users.noreply.github.com>
2021-05-25 13:27:15 -0300

    Add files via upload

---
+++ automacao_aws_python.py
@@ -0,0 +1,76 @@
+
+#Automação AWS
+
+#Introdução ao curso.
+
+#Objetivo do Curso.
+
+# Scripts 
+
+'''
+1. Script - Criando Instâncias EC2 com linguagem de programação Python.
+2. Script - Reduzindo o tamanho de imagens dentro do Amazon S3.
+3. Script - Backup EC2 criar de forma automatizada.
+4. Script - EC2 On e Off ligar e desligar servidores AWS de acordo com o horário programado.
+5. Script - Backup DynamoDB, é feito conforme agendamento.
+
+'''
+
+#O que preciso para o curso.
+#Python e AWS
+
+
+# Aula. 99 Projeto 
+
+
+#  EC2  +  Python 
+'''
+Sempre criar uma Time Line
+1º SSH, Chave Key
+2º Lambda Iam Role, o que pode ser feito com permissões: cloud watch, EC2
+2º Python, executar as variaveis 
+3º EC2, T2. MICRO, IAM, Linux
+
+'''
+
+
+# AULA 100. Criando Instancias EC2.
+
+'''
+Alterar a região
+pra criar o projeto precisa primeiro criar a chave, abra o serviço EC2 e nas instancias...
+sempre que é criado uma instancia é preciso criar uma chave,,,
+
+AWS > EC2 Dashboard > Key Pairs > Create Key Pairs 
+
+nome:ec2-script
+
+formato: ppk
+
+2º passo
+serviços > lambda > AWM Lambda Functions(precisa ficar atento pra não ter custo, então remover e testar até 12 meses)
+
+
+
+Img Verificar a biblioteca Pillow Py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
